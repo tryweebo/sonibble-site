26c5cfa9223579216dc1fe9408ba53b56e35324e
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-21 10:15:16 +0800

    🚜 refactor: change the metadata

--- src/shared/libs/shared-metadata.ts
+++ src/shared/libs/shared-metadata.ts
@@ -2,17 +2,21 @@ import { OpenGraph } from 'next/dist/lib/metadata/types/opengraph-types'
 import { Twitter } from 'next/dist/lib/metadata/types/twitter-types'
 
 const openGraph: OpenGraph = {
-  images: ['YOUR_IMAGE_META_TAG'],
+  images: [
+    'https://cdn.hashnode.com/res/hashnode/image/upload/v1737091364296/0d31c055-5cf4-4a8b-a06d-a1d7d6a6b3c0.png',
+  ],
   locale: 'en_US',
   type: 'website',
-  siteName: 'YOUR_WEBSITE_NAME',
+  siteName: 'Your Business Growth & Success Partner | Sonibble',
 }
 
 const twitter: Twitter = {
-  images: ['YOUR_IMAGE_META_TAG'],
+  images: [
+    'https://cdn.hashnode.com/res/hashnode/image/upload/v1737091364296/0d31c055-5cf4-4a8b-a06d-a1d7d6a6b3c0.png',
+  ],
   card: 'summary_large_image',
-  creator: '@YOUR_TWITTER_HANDLE',
-  site: 'YOUR_WEBSITE_NAME',
+  creator: '@nyomansunima',
+  site: 'Your Business Growth & Success Partner | Sonibble',
 }
 
 export const sharedMetadata = {
