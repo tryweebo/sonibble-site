656b4c097718d05c09ff49d65e379b04e257310d
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-21 10:21:25 +0800

    🚜 refactor: update metadata and import features

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,27 +1,40 @@
 import * as React from 'react'
 import { Metadata } from 'next'
 import { sharedMetadata } from '@shared/libs'
+import {
+  BenefitsSection,
+  FAQSection,
+  HeroSection,
+  ProcessSection,
+  ServiceSection,
+} from '@features/home'
 
 export const metadata: Metadata = {
-  title: 'YOUR_TITLE_META_TAG',
-  description: 'YOUR_DESCRIPTION_META_TAG',
+  title: 'Your Business Growth & Success Partner | Sonibble',
+  description:
+    'We help small business, startups, and professional grow, scale & win the game',
   openGraph: {
     ...sharedMetadata.openGraph,
-    title: 'YOUR_TITLE_META_TAG',
-    description: 'YOUR_DESCRIPTION_META_TAG',
+    title: 'Your Business Growth & Success Partner | Sonibble',
+    description:
+      'We help small business, startups, and professional grow, scale & win the game',
   },
   twitter: {
     ...sharedMetadata.twitter,
-    title: 'YOUR_TITLE_META_TAG',
-    description: 'YOUR_DESCRIPTION_META_TAG',
+    title: 'Your Business Growth & Success Partner | Sonibble',
+    description:
+      'We help small business, startups, and professional grow, scale & win the game',
   },
 }
 
 export default function HomePage(): React.ReactElement {
   return (
     <div className="flex flex-col gap-10">
-      {/* TODO: Importa all of your page section and detail component here */}
-      {/* You can also import from the @features folder */}
+      <HeroSection />
+      <ProcessSection />
+      <BenefitsSection />
+      <ServiceSection />
+      <FAQSection />
     </div>
   )
 }
