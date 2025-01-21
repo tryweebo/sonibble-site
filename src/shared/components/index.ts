b80b5ff307adca43704d68e02741769abeedbe52
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-21 10:18:46 +0800

    🚜 refactor: add logo

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -7,5 +7,6 @@ export * from './common/content'
 export * from './common/centered-layout'
 export * from './common/footer'
 export * from './common/header'
+export * from './common/logo'
 
 export * from './animation/transition'
