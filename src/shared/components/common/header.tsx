6997e59687592293bd17134e2a56e3ddbbd5d66a
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-21 10:17:01 +0800

    🚜 refactor: update the style and links

--- src/shared/components/common/header.tsx
+++ src/shared/components/common/header.tsx
@@ -1,28 +1,67 @@
 'use client'
 import * as React from 'react'
 import Link from 'next/link'
+import { Logo } from './logo'
+import { Button } from '../ui/button'
 
-export function Brand(): React.ReactElement {
+type NavItemProps = {
+  href: string
+  children: React.ReactNode
+}
+
+function Brand(): React.ReactElement {
   return (
-    <div className="text-sm gap-3 flex relative z-10">
-      <Link
-        href={'/'}
-        className="transition-all duration-300 hover:-translate-x-1"
+    <Link
+      href={'/'}
+      className="flex items-center gap-3 transition-all duration-300 hover:-translate-x-1"
+    >
+      <Logo width={32} height={32} />
+      <span>Sonibble</span>
+    </Link>
+  )
+}
+
+function NavItem({ href, children }: NavItemProps): React.ReactElement {
+  return (
+    <Link
+      href={href}
+      className="flex items-center gap-3 transition-all duration-300 text-sm text-foreground/60 hover:text-foreground font-medium"
+    >
+      {children}
+    </Link>
+  )
+}
+
+function Navigation(): React.ReactElement {
+  return <nav className="flex flex-1 gap-6 items-center"></nav>
+}
+
+function Actions(): React.ReactElement {
+  return (
+    <div className="flex relative z-10 gap-2">
+      <Button
+        asChild
+        className="transition-all duration-300 hover:-translate-y-1"
       >
-        <span className="font-medium">yourbrand.domain</span>
-      </Link>
+        <Link
+          href={
+            'https://cal.com/nyomansunima/discovery-call-with-nyoman-sunima-sonibble'
+          }
+          target="_blank"
+        >
+          <i className="fi fi-br-bookmark" />
+          Get started
+        </Link>
+      </Button>
     </div>
   )
 }
 
-export function Actions(): React.ReactElement {
-  return <div className="flex relative z-10 gap-2"></div>
-}
-
 export function Header(): React.ReactElement {
   return (
-    <header className="flex items-center justify-between h-24 tablet:h-36">
+    <header className="flex items-center gap-10 h-24">
       <Brand />
+      <Navigation />
       <Actions />
     </header>
   )
