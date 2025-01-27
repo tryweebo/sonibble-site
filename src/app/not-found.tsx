c138ffa750f79c48ff9df339716db778713edfa8
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-27 18:04:15 +0800

    🚜 refactor: remove important

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,11 +12,11 @@ export default function NotFound(): React.ReactElement {
   return (
     <div className="flex flex-col">
       <section className="flex flex-col items-center py-20">
-        <h1 className="!leading-tight text-3xl tablet:text-4xl text-center font-medium">
+        <h1 className="leading-tight! text-3xl tablet:text-4xl text-center font-medium">
           Not found
         </h1>
 
-        <p className="mt-7 !leading-relaxed laptop:w-10/12 text-center text-foreground">
+        <p className="mt-7 leading-relaxed laptop:w-10/12 text-center text-foreground">
           You’re searching something wrong. This can be happen when the url is
           invalid or deleted, this mean your pages is not found. Start explore
           another one, we help you find better way to navigate through header
