2f101dc06b25ab1d9500457c220e13c5c9a63a3d
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-27 18:04:47 +0800

    🚜 refactor: add icons and remove important

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import '@shared/styles/globals.css'
+import '@flaticon/flaticon-uicons/css/all/all.css'
 import { Button, CenteredLayout } from '@shared/components'
 import { fonts } from '@shared/fonts'
 
@@ -19,11 +20,11 @@ export default function GlobalError({
       <body suppressHydrationWarning>
         <CenteredLayout>
           <main className="min-h-screen py-20 tablet:py-36 tablet:pb-56">
-            <h1 className="!leading-tight text-3xl tablet:text-4xl text-center font-medium">
+            <h1 className="leading-tight! text-3xl tablet:text-4xl text-center font-medium">
               Something wrong!
             </h1>
 
-            <p className="mt-7 !leading-relaxed laptop:w-10/12 text-center text-foreground">
+            <p className="mt-7 leading-relaxed laptop:w-10/12 text-center text-foreground">
               Opps, something happen, don't panic. This can be happen when the
               url is invalid or deleted, this mean your pages is not found.
               Start explore another one, we help you find better way to navigate
