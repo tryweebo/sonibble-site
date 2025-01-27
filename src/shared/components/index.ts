044518019c537739527f73221cb1cbf9311d21c9
Nyoman Sunima <nyomansunima@gmail.com>
2025-01-27 12:40:04 +0800

    🚜 refactor: register section label component

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -8,5 +8,6 @@ export * from './common/centered-layout'
 export * from './common/footer'
 export * from './common/header'
 export * from './common/logo'
+export * from './common/section-label'
 
 export * from './animation/transition'
