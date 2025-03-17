b652c05cf2d09efc5f633efb9907c42309ae91a8
Nyoman Sunima <nyomansunima@gmail.com>
2025-03-17 19:31:13 +0800

    refactor: update the header style and simplify design

--- src/shared/components/common/header.tsx
+++ src/shared/components/common/header.tsx
@@ -4,44 +4,32 @@ import Link from 'next/link'
 import { Logo } from './logo'
 import { Button } from '../ui/button'
 
-function Brand(): React.ReactElement {
-  return (
-    <Link
-      href={'/'}
-      className="flex items-center gap-3 transition-all duration-300 hover:-translate-x-1"
-    >
-      <Logo width={32} height={32} />
-      <span className="font-medium">Sonibble</span>
-    </Link>
-  )
-}
-
-function Actions(): React.ReactElement {
-  return (
-    <div className="flex relative z-10 gap-2">
-      <Button
-        asChild
-        className="transition-all duration-300 hover:-translate-y-1"
-      >
-        <Link
-          href={
-            'https://cal.com/nyomansunima/discovery-call-with-nyoman-sunima-sonibble'
-          }
-          target="_blank"
-        >
-          <i className="fi fi-br-bookmark" />
-          Get started
-        </Link>
-      </Button>
-    </div>
-  )
-}
-
 export function Header(): React.ReactElement {
   return (
     <header className="flex items-center justify-between gap-10 h-24">
-      <Brand />
-      <Actions />
+      <Link
+        href={'/'}
+        className="flex items-center gap-3 transition-all duration-300 hover:-translate-x-1"
+      >
+        <Logo width={30} height={30} />
+        <span className="font-medium font-bricolage">Sonibble.</span>
+      </Link>
+
+      <div className="flex relative z-10 gap-2">
+        <Button
+          asChild
+          variant={'outline'}
+          className="transition-all duration-300 hover:-translate-y-1"
+        >
+          <Link
+            href={'https://cal.com/nyomansunima/sonibble-intro-call'}
+            target="_blank"
+          >
+            Book a call
+            <i className="fi fi-br-video-camera-alt" />
+          </Link>
+        </Button>
+      </div>
     </header>
   )
 }
