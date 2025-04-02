15d0b78a3323ffaa209dd5d114f8d4afc89776c8
Nyoman Sunima <nyomansunima@gmail.com>
2025-04-02 21:07:52 +0800

    refactor: remove dayjs and date util

--- src/shared/utils/helper.ts
+++ src/shared/utils/helper.ts
@@ -1,11 +1,6 @@
-import dayjs from 'dayjs'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function mergeClass(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function parseReadableDate(date: string) {
-  return dayjs(date).format('MMM, DD YYYY')
 }
