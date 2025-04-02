bfdd4ae0e95354f51796124592b1d7c6ce53737d
Nyoman Sunima <nyomansunima@gmail.com>
2025-04-02 21:13:07 +0800

    refactor: add suspense and page view config

--- src/shared/providers/posthog-provider.tsx
+++ src/shared/providers/posthog-provider.tsx
@@ -2,23 +2,56 @@
 
 import * as React from 'react'
 import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
+import { PostHogProvider, usePostHog } from 'posthog-js/react'
 import { config } from '@shared/libs'
+import { usePathname, useSearchParams } from 'next/navigation'
 
 type PosthogProviderProps = {
   children: React.ReactNode
 }
 
-if (typeof window !== 'undefined') {
-  posthog.init(config.posthog.key, {
-    api_host: '/ingest',
-    ui_host: 'https://us.posthog.com',
-    person_profiles: 'identified_only',
-  })
+function PostHogPageView(): null {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const posthog = usePostHog()
+
+  React.useEffect(() => {
+    if (pathname && posthog) {
+      let url = window.origin + pathname
+      if (searchParams.toString()) {
+        url = url + `?${searchParams.toString()}`
+      }
+
+      posthog.capture('$pageview', { $current_url: url })
+    }
+  }, [pathname, searchParams, posthog])
+
+  return null
+}
+
+function SuspendedPostHogPageView(): React.ReactElement {
+  return (
+    <React.Suspense fallback={null}>
+      <PostHogPageView />
+    </React.Suspense>
+  )
 }
 
 export function PosthogProvider({
   children,
 }: PosthogProviderProps): React.ReactElement {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+  React.useEffect(() => {
+    posthog.init(config.posthog.key, {
+      api_host: '/ingest',
+      ui_host: 'https://us.posthog.com',
+      person_profiles: 'identified_only',
+      capture_pageview: false,
+    })
+  }, [])
+  return (
+    <PostHogProvider client={posthog}>
+      <SuspendedPostHogPageView />
+      {children}
+    </PostHogProvider>
+  )
 }
