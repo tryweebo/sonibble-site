ef2fc828b6763e89fca0f9952fa52556896d08d6
Nyoman Sunima <nyomansunima@gmail.com>
2025-04-26 19:35:27 +0800

    refactor: import another sections

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -4,10 +4,12 @@ import { sharedMetadata } from '@shared/libs'
 import { HeroSection } from './hero-section'
 import { ProcessSection } from './process-section'
 import { BenefitsSection } from './benefits-section'
-import { ServiceSection } from './service-section'
+import { ServiceSection } from './service/service-section'
 import { FAQSection } from './faq-section'
 import { CTASection } from './cta-section'
-import { PlanSection } from './plan-section'
+import { PlanSection } from './plans/plan-section'
+import { MissionSection } from './mission-section'
+import { TestimonialSection } from './testimonial-section'
 
 export const metadata: Metadata = {
   title: 'Your Business Growth & Success Partner | Sonibble',
@@ -31,9 +33,11 @@ export default function HomePage(): React.ReactElement {
   return (
     <div className="flex flex-col">
       <HeroSection />
-      <BenefitsSection />
+      <MissionSection />
       <ProcessSection />
       <ServiceSection />
+      <TestimonialSection />
+      <BenefitsSection />
       <PlanSection />
       <FAQSection />
       <CTASection />
