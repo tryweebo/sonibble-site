78eec7d824c67888f4eba03da33df13fdd5a4d64
Nyoman Sunima <nyomansunima@gmail.com>
2025-04-26 19:34:10 +0800

    feat: add dialog component

---
+++ src/shared/components/ui/dialog.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import * as React from 'react'
+import * as DialogPrimitive from '@radix-ui/react-dialog'
+import { mergeClass } from '@shared/utils'
+import { ScrollArea } from './scroll-area'
+
+const Dialog = DialogPrimitive.Root
+const DialogTrigger = DialogPrimitive.Trigger
+const DialogPortal = DialogPrimitive.Portal
+const DialogClose = DialogPrimitive.Close
+
+const DialogOverlay = React.forwardRef<
+  React.ComponentRef<typeof DialogPrimitive.Overlay>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
+>(({ className, ...props }, ref) => (
+  <DialogPrimitive.Overlay
+    ref={ref}
+    className={mergeClass(
+      'fixed inset-0 z-50 bg-black/20 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+      className,
+    )}
+    {...props}
+  />
+))
+DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
+
+const DialogContent = React.forwardRef<
+  React.ComponentRef<typeof DialogPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <DialogPortal>
+    <DialogOverlay />
+    <DialogPrimitive.Content
+      ref={ref}
+      className={mergeClass(
+        'fixed left-[50%] top-[50%] z-50 grid w-[90%] tablet:w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-border bg-background p-2 rounded-3xl duration-300 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-bottom-[48%] data-[state=open]:slide-in-from-bottom-[48%]',
+        className,
+      )}
+      {...props}
+    >
+      <div className="h-full w-full flex flex-col p-6 border-2 border-border border-dashed rounded-2xl">
+        <DialogPrimitive.Title hidden />
+        <ScrollArea className="max-h-[80vh] tablet:max-h-[60vh]">
+          {children}
+        </ScrollArea>
+        <DialogPrimitive.Close className="flex items-center px-3 py-2 rounded-full gap-2 font-medium font-heading absolute right-4 top-4 bg-secondary text-secondary-foreground cursor-pointer text-sm transition-all duration-300 hover:scale-95">
+          Close
+          <i className="fi fi-br-cross-small text-sm" />
+        </DialogPrimitive.Close>
+      </div>
+    </DialogPrimitive.Content>
+  </DialogPortal>
+))
+DialogContent.displayName = DialogPrimitive.Content.displayName
+
+export {
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogClose,
+  DialogTrigger,
+  DialogContent,
+}
