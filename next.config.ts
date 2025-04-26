aef6ff5b7c4b17e739fde70600c2030dd9d3d0a5
Nyoman Sunima <nyomansunima@gmail.com>
2025-04-26 19:33:08 +0800

    refactor: remove extension and experimental features

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,10 @@
 import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizePackageImports: ['@flaticon/flaticon-uicons'],
-  },
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
-    remotePatterns: [
-      { hostname: 'cdn.dribbble.com' },
-      { hostname: 'cdn.hashnode.com' },
-      { hostname: 'images.unsplash.com' },
-      { hostname: 'via.placeholder.com' },
-    ],
+    remotePatterns: [{ hostname: 'cdn.hashnode.com' }],
   },
   skipTrailingSlashRedirect: true,
-  transpilePackages: [],
 }
 
 export default nextConfig
