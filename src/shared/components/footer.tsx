a2673c73cd822de4bc7e867fbac7537c4d06f938
Nyoman Sunima <nyomansunima@gmail.com>
2025-07-19 23:40:04 +0800

    refactor: update icons and content

--- src/shared/components/footer.tsx
+++ src/shared/components/footer.tsx
@@ -4,8 +4,12 @@ export function Footer(): React.ReactElement {
   return (
     <footer className="flex flex-col" id="footer">
       <div className="flex flex-row tablet:items-center py-7 justify-between mt-8">
-        <span className="text-sm cursor-pointer">&copy; Sonibble.</span>
-        <span className="text-sm cursor-pointer">Design in Bali.</span>
+        <span className="text-sm cursor-pointer inline-flex gap-2">
+          Build with <i className="fi fi-sc-heart" />
+        </span>
+        <span className="text-sm cursor-pointer inline-flex">
+          Design in Bali
+        </span>
       </div>
     </footer>
   )
