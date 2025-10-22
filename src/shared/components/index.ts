892f063f436d2aaadcc3c20fdb43e2e06649f4cf
Nyoman Sunima <nyomansunima@gmail.com>
2025-10-22 18:57:57 +0800

    chore: remove file

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,7 +1,5 @@
 export * from "./centered-layout";
 export * from "./content";
-export * from "./footer";
-export * from "./header";
 export * from "./section-label";
 export * from "./ui/accordion";
 export * from "./ui/button";
